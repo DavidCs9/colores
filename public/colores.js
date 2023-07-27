7c566df3f7e6465de9b341556c427a7e3e437b21
DavidCs9 <davidcastrosi@hotmail.com>
2023-07-27 13:49:01 -0600

    Add live preview

--- public/colores.js
+++
@@ -1,8 +0,0 @@
-import { prominent } from "color.js";
-
-console.log("Hello World!");
-const img = document.querySelector("img");
-console.log(img);
-
-// const color = await prominent("agnico-logo.png", { amount: 1 });
-// console.log(color); // [241, 221, 63]
